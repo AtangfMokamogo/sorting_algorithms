33e6050a0bfc0127772639a22bb15f08ff4c15e0
AtangFMokamogo <atangfmokamogo@outlook.com>
2023-02-20 11:09:37 -0800

    fixed typo on file 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -73,7 +73,7 @@ void q_sort(int *array, int low, int high, size_t size)
 
 	if (low < high)
 	{
-		pivot = partition(array, low, high, size);
+		pivot = partitioner(array, low, high, size);
 		q_sort(array, low, pivot - 1, size);
 		q_sort(array, pivot + 1, high, size);
 	}
